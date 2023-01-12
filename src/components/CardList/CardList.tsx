235c530acca206eb575099e4fc1ca6be2c4e7c4d
Andrew <mysecondmail0099@gmail.com>
2023-01-12 21:27:59 +0200

    add highlighting matches for title and body

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable array-callback-return */
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import CardItem from '../CardItem';
 import { CardItemsIcons } from './CardItemsIcons';
 import cl from './cardList.module.scss';
@@ -18,7 +18,7 @@ interface ListTypes {
 
 export const CardList: React.FC = () => {
   const { post, setPost, preview, setPreview, searchQuery } = useMainContext();
-  const [filteredValue, setFilteredValue] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
   // filter function
   const filterCards = (searchText: string, listOfCards: []) => {
@@ -38,11 +38,35 @@ export const CardList: React.FC = () => {
   useEffect(() => {
     const Debounce = setTimeout(() => {
       const filteredCards = filterCards(searchQuery, post);
-      setFilteredValue(filteredCards)
+      setFilteredList(filteredCards)
     }, 300);
 
     return () => clearTimeout(Debounce);
   }, [searchQuery, post])
+
+  // highlight text function
+  const Hightlight = (props: any) => {
+    const { filter, str } = props
+    if (!filter) return str
+    const regexp = new RegExp(filter, 'ig')
+    const matchValue = str.match(regexp)
+
+    if (matchValue) {
+
+      return str.split(regexp).map((s: any, index: any, array: any) => {
+        if (index < array.length - 1) {
+          const c = matchValue.shift()
+          return <>{s}<span className={cl.highlight}>{c}</span></>
+        }
+        return s
+      })
+    }
+    return str
+  }
+
+  const light = useCallback((str: any) => {
+    return <Hightlight filter={searchQuery} str={str} />
+  }, [searchQuery])
 
   // get posts
   useEffect(() => {
@@ -60,33 +84,37 @@ export const CardList: React.FC = () => {
 
   return (
     <>
-      <p className={cl.result}>Results: {filteredValue.length}</p>
-      <div className={cl.list}>
-        {filteredValue.map((item: ListTypes) => (
-          <CardItem
-            key={item.id}
-          >
-            {preview.map((prev: ListTypes) => (
-              (prev.id === item.id) && (
-                <img className={cl.item_img} key={prev.id} src={prev?.url} alt="" />
-              )
-            ))}
-            <div className={cl.item_content}>
-              <div className={cl.item_content_top}>
-                <p>{item.title}</p>
-                <span>{`${item.body.slice(0, 100)}...`}</span>
+      <p className={cl.result}>Results: {filteredList.length}</p>
+      {filteredList.length ? (
+        <div className={cl.list}>
+          {filteredList.map((item: ListTypes) => (
+            <CardItem
+              key={item.id}
+            >
+              {preview.map((prev: ListTypes) => (
+                (prev.id === item.id) && (
+                  <img className={cl.item_img} key={prev.id} src={prev?.url} alt="" />
+                )
+              ))}
+              <div className={cl.item_content}>
+                <div className={cl.item_content_top}>
+                  <p>{light(item.title)}</p>
+                  <span>{light(item.body.slice(0, 100) + "...")}</span>
+                </div>
+                <Link
+                  to={`/details/${item.id}`}
+                  className={cl.read_more}
+                >
+                  <p>Read more</p>
+                  <CardItemsIcons id="more" />
+                </Link>
               </div>
-              <Link
-                to={`/details/${item.id}`}
-                className={cl.read_more}
-              >
-                <p>Read more</p>
-                <CardItemsIcons id="more" />
-              </Link>
-            </div>
-          </CardItem>
-        ))}
-      </div>
+            </CardItem>
+          ))}
+        </div>
+      ) : (
+        <h2>Sorry, no matches :(</h2>
+      )}
     </>
   )
 }
